5e631f6ee998fda5a72fdfe14aa86af7ed4cb635
2Hasan2 <h01554176846@gmail.com>
2024-05-31 22:14:34 +0300

    2024-05-31 10:14:34 PM

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -30,21 +30,22 @@ function getPlaces () {
     url: PLACES_URL,
     type: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify({}),
+    data: JSON.stringify({ amenities: Object.values(amenityObj) }),
     success: function (response) {
-      for (const articleData of response) {
+      $('SECTION.places').empty();
+      for (const r of response) {
         const article = ['<article>',
           '<div class="title_box">',
-        `<h2>${articleData.name}</h2>`,
-        `<div class="price_by_night">$${articleData.price_by_night}</div>`,
+        `<h2>${r.name}</h2>`,
+        `<div class="price_by_night">$${r.price_by_night}</div>`,
         '</div>',
         '<div class="information">',
-        `<div class="max_guest">${articleData.max_guest} Guest(s)</div>`,
-        `<div class="number_rooms">${articleData.number_rooms} Bedroom(s)</div>`,
-        `<div class="number_bathrooms">${articleData.number_bathrooms} Bathroom(s)</div>`,
+        `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
+        `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
+        `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
         '</div>',
         '<div class="description">',
-        `${articleData.description}`,
+        `${r.description}`,
         '</div>',
         '</article>'];
         $('SECTION.places').append(article.join(''));
